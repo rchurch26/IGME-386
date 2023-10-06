414c697c4bdaee37ec085a4aa675a30632b02af8
rchurch26 <rahmelchurch@gmail.com>
2023-10-05 20:10:40 -0400

    Completed simple python program

---
+++ simple-python-program.py
@@ -0,0 +1,9 @@
+helloStr = "Hello"
+worldStr = "World"
+# greeting = helloStr + " " + worldStr
+# print(greeting)
+print(helloStr + " " + worldStr)
+
+feet = ["100 feet", "500 feet", "750 feet"]
+for list in feet: 
+    print("roads_" + list[:3])
